cdcadc6f3b808d905dc3b7d207a20679a5c89ee4
Tonji <tonjiz12@gmail.com>
2017-04-28 12:56:10 -0400

    Update for requesting trips

--- ElevatorSystem/src/tz/challenge/Elevator.java
+++ ElevatorSystem/src/tz/challenge/Elevator.java
@@ -6,39 +6,47 @@ public class Elevator {
 	private int currentFloor;
 	private int numberOfTrips;
 	private int numberOfFloors;
+	private boolean moveUpOrDown;
 	private boolean occupied;
-	
+
 	public Elevator(int elevatorId) {
 		this.elevatorId = elevatorId;
+	}
+
+	public int getElevatorId() {
+		return elevatorId;
 	}
 	
 	public int getCurrentFloor() {
 		return currentFloor;
 	}
+
 	public void setCurrentFloor(int currentFloor) {
 		this.currentFloor = currentFloor;
 	}
+
 	public int getNumberOfTrips() {
 		return numberOfTrips;
 	}
+
 	public void setNumberOfTrips(int numberOfTrips) {
 		this.numberOfTrips = numberOfTrips;
 	}
+
 	public int getNumberOfFloors() {
 		return numberOfFloors;
 	}
+
 	public void setNumberOfFloors(int numberOfFloors) {
 		this.numberOfFloors = numberOfFloors;
 	}
+
 	public boolean isOccupied() {
 		return occupied;
 	}
+
 	public void setOccupied(boolean occupied) {
 		this.occupied = occupied;
 	}
-	public int getElevatorId() {
-		return elevatorId;
-	}
-	
-	
+
 }
--- ElevatorSystem/src/tz/challenge/ElevatorController.java
+++ ElevatorSystem/src/tz/challenge/ElevatorController.java
@@ -1,12 +1,56 @@
 package tz.challenge;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class ElevatorController {
 
+	private final static int MAX_TRIPS = 100;
+	private final static int GROUND_FLOOR = 1;
+	
 	private List<Elevator> elevators;
 	private Queue<TripRequest> requests;
 	private int numOfFloors;
+
+	public ElevatorController(int numberOfElevators, int numberOfFloors) {
+		this.numOfFloors = numberOfFloors;
+		this.elevators = new ArrayList<Elevator>();
+		this.requests = new LinkedList<TripRequest>();
+		initializeElevators(numberOfElevators);
+		
+	}
 	
+	public void addRequest(TripRequest request) {
+		requests.add(request);
+	}
+	
+	public void removeRequest(TripRequest request) {
+		requests.poll();
+	}
+	
+	public void processTripRequest(int floorFrom, int floorTo) {
+		TripRequest newRequest = new TripRequest(floorFrom, floorTo);
+		addRequest(newRequest);
+		
+		Elevator closestElevator = findClosestElevator();
+	}
+	
+	
+	private Elevator findClosestElevator() {
+		Elevator elevator = elevators.get(0);  //set first elevator as default
+		
+		return elevator;
+		
+	}
+
+	private void initializeElevators(int numberOfElevators) {
+		for (int i = 0; i < numberOfElevators; i++) {
+			elevators.add(new Elevator(i + 1));
+		}
+	}
+	
+	
+
 }
